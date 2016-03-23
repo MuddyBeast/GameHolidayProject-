2a70772aefcb88ccbb43766d0600db505a32377f
Snoodelz <ludwig.mostrom@gmail.com>
2016-03-23 19:53:18 +0100

    Update

---
+++ Assets/DistanceToPlayer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceToPlayer : MonoBehaviour 
+{
+	GameObject player;
+	float distance;
+	Vector2 PlatformUpPosition;
+	Vector2 platformDownPosition;
+	bool platformDown = false;
+	public float distanceForPlatformToAppear = 4.3f;
+	public float delayForPlatform = 3f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		PlatformUpPosition = transform.position;
+		platformDownPosition = new Vector2(transform.position.x, transform.position.y - 11f);
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		distance = Vector2.Distance(platformDownPosition, player.transform.position);
+
+	}
+
+
+	void FixedUpdate()
+	{
+		if (distance < 4.3f)
+			TakeDownPlatform ();
+		else
+			TakeUpPlatform ();
+	}
+
+	void TakeDownPlatform()
+	{
+		transform.position = Vector2.Lerp (transform.position, platformDownPosition, delayForPlatform * Time.deltaTime);
+	}
+
+	void TakeUpPlatform()
+	{
+		transform.position = Vector2.Lerp (transform.position, PlatformUpPosition, delayForPlatform * Time.deltaTime);
+	}
+}
--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -19,33 +19,68 @@ public class MovementScript : MonoBehaviour
     {
         Debug.Log(currentSpeed);
     }
-    void FixedUpdate()
+    
+	void FixedUpdate()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            rb.velocity = Vector2.right * currentSpeed;
-            if (maxSpeed > currentSpeed)
-            {
-                currentSpeed += movementSpeedIncrease;
-                if (currentSpeed > maxSpeed)
-                    currentSpeed = maxSpeed;
-            }
-        }
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            rb.velocity = Vector2.left * currentSpeed;
-            if (maxSpeed > currentSpeed)
-            {
-                currentSpeed += movementSpeedIncrease;
-                if (currentSpeed > maxSpeed)
-                    currentSpeed = maxSpeed;
-            }
-        }
-        else
+		Vertical ();
+		Horizontal ();
+
+
+
+		if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
         {
             rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, 5f * Time.deltaTime);
             currentSpeed = startingSpeed;
         }
+
            
     }
+
+	void Horizontal()
+	{
+		if (Input.GetAxisRaw ("Horizontal") > 0) 
+		{
+			rb.velocity = Vector2.right * currentSpeed;
+			if (maxSpeed > currentSpeed) 
+			{
+				currentSpeed += movementSpeedIncrease;
+				if (currentSpeed > maxSpeed)
+					currentSpeed = maxSpeed;
+			}
+		} 
+		else if (Input.GetAxisRaw ("Horizontal") < 0) 
+		{
+			rb.velocity = Vector2.left * currentSpeed;
+			if (maxSpeed > currentSpeed) 
+			{
+				currentSpeed += movementSpeedIncrease;
+				if (currentSpeed > maxSpeed)
+					currentSpeed = maxSpeed;
+			}
+		}
+	}
+
+
+	void Vertical()
+	{
+		if (Input.GetAxisRaw ("Vertical") > 0) 
+		{
+			rb.velocity = Vector2.up * currentSpeed;
+			if (maxSpeed > currentSpeed) 
+			{
+				currentSpeed += movementSpeedIncrease;
+				if (currentSpeed > maxSpeed)
+					currentSpeed = maxSpeed;
+			}
+		} else if (Input.GetAxisRaw ("Vertical") < 0) 
+		{
+			rb.velocity = Vector2.down * currentSpeed;
+			if (maxSpeed > currentSpeed) 
+			{
+				currentSpeed += movementSpeedIncrease;
+				if (currentSpeed > maxSpeed)
+					currentSpeed = maxSpeed;
+			}
+		}
+	}
 }
